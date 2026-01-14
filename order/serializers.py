3d91dc3c5a3b09b0b9573e2eb43b54851d8eda42
Genstone1805 <88464607+Genstone1805@users.noreply.github.com>
2026-01-14 15:07:18 +1400

    Bug Fixes

--- order/serializers.py
+++ order/serializers.py
@@ -5,7 +5,7 @@ from .models import GiftCardOrder
 class GiftCardOrderCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = GiftCardOrder
-        fields = ["id", 'type', 'name', 'image', 'amount']
+        fields = ["id", 'type', 'card', 'image', 'amount', "e_code_pin"]
 
     def validate_amount(self, value):
         if value <= 0:
