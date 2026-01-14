7311773b230e4a73d8a57008d94ba928fdaaa4c5
Genstone1805 <88464607+Genstone1805@users.noreply.github.com>
2026-01-14 14:06:40 +1400

    Bug Fixes

--- cards/serializers.py
+++ cards/serializers.py
@@ -4,14 +4,14 @@ from cards.models import GiftCardStore, GiftCardNames
 class GiftCardNameSerializer(serializers.ModelSerializer):
   class Meta:
     model = GiftCardNames
-    fields = ["name", "type", "rate"]
+    fields = ["id", "name", "type", "rate"]
 
 class GiftCardStoreListSerializer(serializers.ModelSerializer):
   cards = serializers.SerializerMethodField()
   image = serializers.ImageField(use_url=True)
   class Meta:
     model = GiftCardStore
-    fields = ["category", "name", "image", "cards"]
+    fields = ["id", "category", "name", "image", "cards"]
 
   def get_cards(self, obj):
     cards = GiftCardNames.objects.filter(store=obj)
