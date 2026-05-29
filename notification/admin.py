990573485672654589644b62edf28265d2dcc08f
Genstone1805 <88464607+Genstone1805@users.noreply.github.com>
2026-05-30 07:29:33 +1400

    make the push notifiaction visible to the admin dashboard

--- notification/admin.py
+++ notification/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Notification, NotificationEvent
-
+from .models import Notification, NotificationEvent, PushNotificationToken
 
 admin.site.register(Notification)
 admin.site.register(NotificationEvent)
+admin.site.register(PushNotificationToken)
